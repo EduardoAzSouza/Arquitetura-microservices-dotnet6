5f3e2c2fd23c599ed83e4cfd8695704bea742063
EduardoAzSouza <EduardoAzSouza@github.com>
2023-01-13 00:41:54 -0300

    Creating GeekShopping.CouponAPI Microservice

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -12,7 +12,9 @@ builder.Services.AddHttpClient<IProductService, ProductService>(c =>
 builder.Services.AddHttpClient<ICartService, CartService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"])
 );
-
+//services.AddHttpClient<ICouponService, CouponService>(c =>
+//        c.BaseAddress = new Uri(Configuration["ServiceUrls:CouponAPI"])
+//    );
 
 
 builder.Services.AddControllersWithViews();
